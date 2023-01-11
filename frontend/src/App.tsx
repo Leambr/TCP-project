5eb97fc49ec2f7c9f2debd99c9d6cf07d397bc01
Léa M <mhoumadi.lea@gmail.com>
2023-01-11 15:05:54 +0100

    style: create variables css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,39 @@
 import "./App.css";
+import "./design-system/styles/grid.css";
+import "./design-system/styles/layout.css";
 
 function App() {
     return (
-        <div className="App">
-            <h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1>
+        <div className="layout-with-centered-content">
+            <section className="main-container">
+                <div className="col-12 mb-40">
+                    <h1 className="title">Hello</h1>
+                </div>
+                <div className="col-6 mb-40">
+                    <h1>Hello</h1>
+                    <h1>Hello</h1>
+                </div>
+            </section>
         </div>
+
+        // Exemple d'utilisation de la grid et du layout pour les composants
+        // <div className="layout-with-centered-content">
+        //           <section className="main-container">
+        //               <div className="col-12 mb-40"></div>
+        //           </section>
+        //           <section className="main-container">
+        //               {openCart && <Cart onHideCart={hideCartHandler} />}
+        //           </section>
+        //           <section className="main-container">
+        //               <div className="col-6">
+        //                   <Card>
+        //                       {mealCardData.map((meal) => {
+        //                           return <MealCard key={meal.id} meal={meal} />;
+        //                       })}
+        //                   </Card>
+        //               </div>
+        //           </section>
+        //   </div>
     );
 }
 
